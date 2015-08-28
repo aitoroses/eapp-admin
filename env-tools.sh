b33e31d00b1d46a80e61b60118a44d31713d5bc7
Aitor Oses <aitor.oses@gmail.com>
2015-08-28 14:58:01 +0200

    Env tools for vagrant

---
+++ env-tools.sh
@@ -0,0 +1,5 @@
+# Install development tools
+sudo apt-get install -y git curl make build-essentials
+curl https://raw.githubusercontent.com/creationix/nvm/v0.23.2/install.sh | bash
+source ~/.nvm/nvm.sh
+nvm install iojs
