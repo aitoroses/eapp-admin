19cfadb4e3045810ad8436206f37e02711f9f7b1
Aritz Muniain Sanchez <aritz.muniain@bpmsoasolutions.com>
2015-08-28 13:06:46 +0200

    Modified mock-server to work on windows (without using Path.join)

--- test/mock-server.js
+++ test/mock-server.js
@@ -1,5 +1,4 @@
-var Nocker = require('nocker');
-var path = require('path');
+var Nocker = require('nocker')
 
 var itemsCollection = require('./mock-db/items')
 var tasCollection = require('./mock-db/tas')
@@ -14,25 +13,25 @@ function getRest(collection, pathname, key) {
   return [
     {
       method: 'POST',
-      path: path.join("/", pathname, 'selectbyexample'), //?firstResult=0&maxResults=10'
+      path: '/' + pathname + '/selectbyexample', //?firstResult=0&maxResults=10'
       reply(params, query, body) {
         collection.find(body).sort({[key]: 1}).exec(function(err, items) {
           var result = query.firstResult ? items.slice(query.firstResult, query.firstResult + query.maxResults) : items
           this.res.json(result)
-        }.bind(this));
+        }.bind(this))
       }
     }, {
       method: 'POST',
-      path: path.join("/", pathname, 'count'),
+      path: '/' + pathname + '/count',
       reply(params, query, body) {
         collection.find(body, function(err, items) {
           this.res.json(items.length)
-        }.bind(this));
+        }.bind(this))
       }
     },
     {
       method: 'PUT',
-      path: path.join("/", pathname, 'update'),
+      path: '/' + pathname + '/update',
       reply(params, query, body) {
         collection.update({itemId: body.itemId}, body, function(err, item) {
           if (err) {
@@ -40,12 +39,12 @@ function getRest(collection, pathname, key) {
           } else {
             this.res.json(true)
           }
-        }.bind(this));
+        }.bind(this))
       }
     },
     {
       method: 'POST',
-      path: path.join("/", pathname, 'insert'),
+      path: '/' + pathname + '/insert',
       reply(params, query, body) {
         collection.insert(body, function(err, item) {
           if (err) {
@@ -53,15 +52,15 @@ function getRest(collection, pathname, key) {
           } else {
             this.res.json(true)
           }
-        }.bind(this));
+        }.bind(this))
       }
     }
   ]
 }
 
-Nocker.register(getRest(itemsCollection, "eappservices/flowstepitem/confitem", "itemId"))
-Nocker.register(getRest(tasCollection, "np5services/countryandta/np5therapeuticalarea", "taId"))
+Nocker.register(getRest(itemsCollection, 'eappservices/flowstepitem/confitem', 'itemId'))
+Nocker.register(getRest(tasCollection, 'np5services/countryandta/np5therapeuticalarea', 'taId'))
 
 Nocker.start({port: 7003}, function() {
-  console.log("Listening on port 7003")
+  console.log('Listening on port 7003')
 })
